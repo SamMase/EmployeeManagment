c00a88f57b2282d23f53aa34232ae1c05c74709d
Shubham Srivastava <smjackson94@gmail.com>
2015-08-24 23:40:22 +0530

    Updated..
    Kardiya be le Verma.

--- src/Payroll.java
+++ src/Payroll.java
@@ -1,4 +1,18 @@
 public class Payroll {
-	
+	public static void showEmployee(Employee tempEmployee) {
+		
+		if(tempEmployee instanceof Employee)
+			System.out.println("Name\t=\t" + tempEmployee.getFirstName() + " " + tempEmployee.getLastName()
+					+ "\nCompany ID\t=\t" + tempEmployee.getEmployeeMainId()
+					+ "\nMain Salary\t=\t" + tempEmployee.getSalary()
+					+ "\nEmail ID\t=\t" + tempEmployee.getEmailId());
+		
+		if(tempEmployee instanceof SalesPerson)
+			System.out.println("Employees Captured\t=\t" + ((SalesPerson)tempEmployee).getNumberOfCustomersCaptured());
+		
+		if(tempEmployee instanceof Developer)
+			System.out.println("Current Project\t=\t" + ((Developer)tempEmployee).getCurrentProject());
+		
+	}	
 	
 }
